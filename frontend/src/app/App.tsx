32e87e636c5cb472cc39d7735f429a476063e357
Kacper Gromala <Kacper.Gromala9@gmail.com>
2026-05-11 12:13:29 +0200

    chore: replace axios with ky and add react-query

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,23 +1,26 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { AuthProvider } from '../features/auth/context/AuthProvider'
 import { AppShell } from './layout/AppShell'
-import { HomePage } from '../features/auth/pages/HomePage'
+import { HomePage } from '../pages/HomePage'
 import { LoginPage } from '../features/auth/pages/LoginPage'
 import { RegisterPage } from '../features/auth/pages/RegisterPage'
+import { QueryProvider } from './providers/QueryProvider'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route element={<AppShell />}>
-            <Route index element={<HomePage />} />
-            <Route path="login" element={<LoginPage />} />
-            <Route path="register" element={<RegisterPage />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Route>
-        </Routes>
-      </AuthProvider>
+      <QueryProvider>
+        <AuthProvider>
+          <Routes>
+            <Route element={<AppShell />}>
+              <Route index element={<HomePage />} />
+              <Route path="login" element={<LoginPage />} />
+              <Route path="register" element={<RegisterPage />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </QueryProvider>
     </BrowserRouter>
   )
 }
